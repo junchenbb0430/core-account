4cf4127a2c98f61d1b81d981e85494199f19cdc9
lijunshi <lijunshi@ehomepay.com.cn>
2020-07-08 07:54:53 +0800

    第一次提交代码工程

---
+++ account-web/src/main/java/com/egf/finacial/account/web/AccountApplication.java
@@ -0,0 +1,21 @@
+package com.egf.finacial.account.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lijunshi
+ * @create 2020-06-23 11:53
+ */
+@EnableAutoConfiguration
+@SpringBootApplication
+public class AccountApplication {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(AccountApplication.class,args);
+    }
+
+}
+
