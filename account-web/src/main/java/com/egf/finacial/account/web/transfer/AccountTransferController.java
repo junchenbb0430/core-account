12a60e9062056001d2cf79c6c59c98c5705c4331
lijunshi <lijunshi@ehomepay.com.cn>
2020-07-08 18:13:03 +0800

    添加修改文件夹

---
+++ account-web/src/main/java/com/egf/finacial/account/web/transfer/AccountTransferController.java
@@ -0,0 +1,10 @@
+package com.egf.finacial.account.web.transfer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/transfer")
+public class AccountTransferController {
+    private static final Logger logger  = LoggerFactory.getLogger(AccountTransferController.class);
+}
