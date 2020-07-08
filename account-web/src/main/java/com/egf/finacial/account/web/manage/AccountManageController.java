5a8e9c09a2215df2c9227280dd95c6b08e9c861a
lijunshi <lijunshi@ehomepay.com.cn>
2020-07-09 00:01:49 +0800

    添加账户类型和账户管理服务功能

---
+++ account-web/src/main/java/com/egf/finacial/account/web/manage/AccountManageController.java
@@ -0,0 +1,9 @@
+package com.egf.finacial.account.web.manage;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/acctManage")
+@RestController
+public class AccountManageController {
+}
---
+++ account-web/src/main/java/com/egf/finacial/account/web/manage/AccountTypeManageController.java
@@ -0,0 +1,22 @@
+package com.egf.finacial.account.web.manage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/typeManage")
+@RestController
+public class AccountTypeManageController {
+
+    private  static final Logger logger = LoggerFactory.getLogger(AccountTypeManageController.class);
+
+    public String  addAccountType(@RequestBody  String  acctTypeReq){
+        return  null;
+    }
+
+    public String  modifyAccountType(@RequestBody String acctTypeReq){
+        return  null;
+    }
+}
