bda2303cc31b440a2b4c814571dae89cb7f84ffc
lijunshi <lijunshi@ehomepay.com.cn>
2020-07-08 18:37:45 +0800

    添加转账接口

--- account-web/src/main/java/com/egf/finacial/account/web/transfer/AccountTransferController.java
+++ account-web/src/main/java/com/egf/finacial/account/web/transfer/AccountTransferController.java
@@ -2,9 +2,28 @@ package com.egf.finacial.account.web.transfer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/transfer")
 public class AccountTransferController {
     private static final Logger logger  = LoggerFactory.getLogger(AccountTransferController.class);
+
+    @RequestMapping("/inBank")
+    public String  transferInBank(@RequestBody String transferReq){
+
+        return null;
+    }
+
+
+    /**
+     * 跨行转账请求
+     * @param transferReq
+     * @return
+     */
+    @RequestMapping("/crossBank")
+    public String transferCrossBank(@RequestBody String transferReq){
+
+        return  null;
+    }
 }
