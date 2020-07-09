2877dfaf9a734327bbd236abb9fab8c2a585f456
lijunshi <lijunshi@ehomepay.com.cn>
2020-07-09 10:17:26 +0800

    添加账户强制冻结和解冻功能

--- account-web/src/main/java/com/egf/finacial/account/web/manage/AccountManageController.java
+++ account-web/src/main/java/com/egf/finacial/account/web/manage/AccountManageController.java
@@ -1,9 +1,25 @@
 package com.egf.finacial.account.web.manage;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/acctManage")
 @RestController
 public class AccountManageController {
+
+    /**
+     * 强制冻结账户
+     * @param acctReq
+     * @return
+     */
+    @RequestMapping("/forceFreeze")
+    public  String  forceFreezeAccount(@RequestBody String acctReq){
+        return  null;
+    }
+
+    @RequestMapping("/unfreeze")
+    public String  unfreezeAccount(@RequestBody String acctReq){
+        return null;
+    }
 }
